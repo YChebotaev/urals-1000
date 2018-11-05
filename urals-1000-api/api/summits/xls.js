9b500d3e8dc1057447891cfc0bcb5049d5ef77a0
Yury Chebotaev <yury.79120345101@gmail.com>
2018-11-05 21:11:11 +0100

    Support for xls uploading

--- urals-1000-api/api/summits/xls.js
+++ urals-1000-api/api/summits/xls.js
@@ -1,32 +1,64 @@
 const Summit = require('../../model/Summit')
 const path = require('path')
 const fs = require('fs')
+const XLSX = require('xlsx')
+const shortid = require('shortid')
+
+const startProcessing = async filePath => {
+  const workbook = XLSX.readFile(filePath)
+  const data = XLSX.utils.sheet_to_json(
+    workbook.Sheets[workbook.SheetNames[0]],
+    {
+      headers: 1
+    }
+  )
+  return data.map(row => ({
+      name: row['Название'],
+      height: row['Высота'],
+      ridge: row['Хребет'],
+      coords: [
+        row['Широта'],
+        row['Долгота']
+      ]
+  }))
+}
 
 module.exports = async (req, res) => {
   const filePath = path.resolve(
-    path.join(__dirname, '../../statics/summits.xls')
+    path.join(__dirname, `../../statics/${shortid.generate()}.xls`)
   )
 
+  const writeQueue = []
   await new Promise((resolve, reject) => {
-    const onData = function(data) {
-      fs.appendFile(filePath, data, function(err) {
-        if (err) {
-          console.error(err)
-          reject(err)
-        }
-      })
-    }
-
-    const onEnd = function() {
-      req.off('data', onData)
-      req.off('end', onEnd)
-      resolve()
+    const onData = data => {
+      const promise = new Promise((resolve, reject) =>
+        fs.appendFile(filePath, data, error => error ? reject(error) : resolve())
+      )
+      writeQueue.push(promise)
     }
 
     req.on('data', onData)
-    req.on('end', onEnd)
-    req.on('error', reject)
+    req.once('end', () => {
+      req.off('data', onData)
+      resolve()
+    })
+    req.once('error', reject)
   })
+
+  await Promise.all(writeQueue)
+  const data = await startProcessing(filePath)
+
+  for (const row of data) {
+    let summit = await Summit.findOne({
+      name: row.name
+    })
+    if (summit) {
+      Object.assign(summit, row)
+    } else {
+      summit = new Summit(row)
+    }
+    await summit.save()
+  }
   
-  res.end()
+  res.json(data)
 }
