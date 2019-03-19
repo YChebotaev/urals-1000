6c8bd5b30060dda3503628f6d8ca820d933c7239
Yury Chebotaev <yury.79120345101@gmail.com>
2019-03-19 18:24:34 +0500

    Fix typo

--- urals-1000-api/api/summits/addClimb.js
+++ urals-1000-api/api/summits/addClimb.js
@@ -1,22 +1,22 @@
-const mongoose = require('mongoose')
-const Summit = require('../../model/Summit')
-const User = require('../../model/user')
-const Climb = require('../../model/Climb')
+const mongoose = require("mongoose");
+const Summit = require("../../model/Summit");
+const User = require("../../model/User");
+const Climb = require("../../model/Climb");
 
 const DEFAULT_CLIMB_AVATAR = {
-  "url" : "http://1000.southural.ru/static/climber_sm.png",
-  "width" : 50,
-  "height" : 50
-}
+  url: "http://1000.southural.ru/static/climber_sm.png",
+  width: 50,
+  height: 50
+};
 
 module.exports = async (req, res) => {
   const user = await User.findOne({
     _id: req.user._id
-  })
-  
+  });
+
   const summit = await Summit.findOne({
     _id: mongoose.Types.ObjectId.createFromHexString(req.params.id)
-  })
+  });
 
   const climb = new Climb({
     ...req.body,
@@ -24,21 +24,21 @@ module.exports = async (req, res) => {
     comment: req.body.comment,
     user: user._id,
     summit: summit._id
-  })
+  });
 
-  user.climbs.push(climb)
-  summit.climbs.push(climb)
+  user.climbs.push(climb);
+  summit.climbs.push(climb);
 
-  const { imageUrls } = req.body
+  const { imageUrls } = req.body;
   imageUrls.forEach(imageUrl => {
     summit.images.push({
       url: imageUrl
-    })
-  })
+    });
+  });
 
-  await climb.save()
-  await user.save()
-  await summit.save()
+  await climb.save();
+  await user.save();
+  await summit.save();
 
-  res.json(climb)
-}
+  res.json(climb);
+};
--- urals-1000-frontend/src/components/TopBar.js
+++ urals-1000-frontend/src/components/TopBar.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { withRouter } from 'react-router-dom'
-import { Navbar, Nav, NavItem } from 'react-bootstrap'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+import { Navbar, Nav, NavItem } from "react-bootstrap";
 
 class TopBarMarkup extends Component {
   static propTypes = {
@@ -9,30 +9,47 @@ class TopBarMarkup extends Component {
       path: PropTypes.string
     }),
     withMargin: PropTypes.bool
-  }
+  };
 
   isActive(path) {
-    return path === this.props.match.path
+    return path === this.props.match.path;
   }
 
   render() {
     return (
       <div className="TopBar">
-        <Navbar staticTop className={`TopBar__navbar ${this.props.withMargin ? '' : 'TopBar__navbar--withoutMargin'}`}>
-          <Navbar.Header className='TopBar__header'>
-            <Navbar.Brand className='TopBar__brand'>Вершины-тысячники Среднего Урала</Navbar.Brand>
+        <Navbar
+          staticTop
+          className={`TopBar__navbar ${
+            this.props.withMargin ? "" : "TopBar__navbar--withoutMargin"
+          }`}
+        >
+          <Navbar.Header className="TopBar__header">
+            <Navbar.Brand className="TopBar__brand">
+              Вершины-тысячники Среднего Урала
+            </Navbar.Brand>
           </Navbar.Header>
           <Nav pullRight>
-            <NavItem href="/" active={this.isActive('/')}>Карта</NavItem>
-            <NavItem href="/summits" active={this.isActive('/summits')}>Вершины</NavItem>
-            <NavItem href="/climbs" active={this.isActive('/climbs')}>Восходители</NavItem>
-            <NavItem href="/about" active={this.isActive('/about')}>О проекте</NavItem>
-            <NavItem href="/profile" active={this.isActive('/profile')}>Профиль</NavItem>
+            <NavItem href="/" active={this.isActive("/")}>
+              Карта
+            </NavItem>
+            <NavItem href="/summits" active={this.isActive("/summits")}>
+              Вершины
+            </NavItem>
+            <NavItem href="/climbs" active={this.isActive("/climbs")}>
+              Восходители
+            </NavItem>
+            <NavItem href="/about" active={this.isActive("/about")}>
+              О проекте
+            </NavItem>
+            <NavItem href="/profile" active={this.isActive("/profile")}>
+              Профиль
+            </NavItem>
           </Nav>
         </Navbar>
       </div>
-    )
+    );
   }
 }
 
-export const TopBar = withRouter(TopBarMarkup)
+export const TopBar = withRouter(TopBarMarkup);
